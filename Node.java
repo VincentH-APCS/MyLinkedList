c7871087fa67c17472198cd6734bdd24d13d9989
VincentH-APCS <vhuang10@stuy.edu>
2021-01-06 13:11:31 -0500

    Wrote out a method for get and set, and changed the constructor so it makes next and prev null by default-unsure if that's right or not though

--- Node.java
+++ Node.java
@@ -1,7 +1,18 @@
 public class Node{
   public Node(String value){
     data = value;
+    next = null;
+    prev = null;
   }
   private String data;
   private Node next,prev;
+
+  public String get(){
+    return data;
+  }
+  public String set(String value){
+    String old = data;
+    data = value;
+    return old;
+  }
 }
