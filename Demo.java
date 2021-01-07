3302bb99e35c88e1ef651a4c5da2794b3e2215b1
VincentH-APCS <vhuang10@stuy.edu>
2021-01-07 00:18:32 -0500

    Still had issues compiling, there was a miscount in the index the LinkedList was up to

--- Demo.java
+++ Demo.java
@@ -14,11 +14,12 @@ public class Demo{
     Snake.add(4, "He then went to");
     Snake.add(4, "at McWizardry Bell");
     Snake.add(0, "This story is an old one.");
-    Snake.add(8, "the Wands R Us");
-    Snake.add(9, "That was next door");
+    Snake.add(7, "the Wands R Us");
+    Snake.add(8, "That was next door");
     System.out.println(Snake.size());
-    Snake.add(10, "So he could");
-    Snake.add(11, "purchase 2 new wands");
+    Snake.add(9, "So he could");
+    Snake.add(10, "purchase");
+    Snake.add(11, "2 new wands");
     Snake.add(12, "one for himself");
     Snake.add(13, "and one for his");
     Snake.add(14, "niece's birthday next week.");
