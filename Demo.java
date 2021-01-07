3e9c306648650af6a97046b4409f80bfc927c25e
VincentH-APCS <vhuang10@stuy.edu>
2021-01-06 23:57:07 -0500

    Testing a bit of the set method

--- Demo.java
+++ Demo.java
@@ -7,25 +7,37 @@ public class Demo{
     MyLinkedList Snake = new MyLinkedList();
 
     System.out.println(Snake.size());
-    System.out.println(Snake.add("alpha"));
-    System.out.println(Snake.add("beta"));
-    System.out.println(Snake.size());
-    System.out.println(Snake.add("Omega!"));
+    System.out.print(Snake.add("alpha  "));
+    System.out.print(Snake.add("beta  "));
+    System.out.print(Snake.size());
+    System.out.println(Snake.add("Omega!  "));
     System.out.print(Snake.get(0) + "  ");
     System.out.print(Snake.get(1) + "  ");
     System.out.print(Snake.get(2) + "   ");
     //System.out.println(Snake.get(3));
-    Snake.add(2, "Whoa There Buddy!");
-    System.out.println(Snake.size());
-    System.out.print(Snake.get(0) + "  ");
-    System.out.print(Snake.get(1) + "  ");
-    System.out.print(Snake.get(2) + "  ");
-    System.out.println(Snake.get(3) + "  ");
-  //  System.out.println(Snake.get(-3));
+    Snake.add(2, "Whoa There Buddy!  ");
+    System.out.println();
+    System.out.print(Snake.size());
+    System.out.print(Snake.get(0));
+    System.out.print(Snake.get(1));
+    System.out.print(Snake.get(2));
+    System.out.print(Snake.get(3));
+  System.out.println();
     Snake.add(0, "I want to lead   ");
-    System.out.println(Snake.get(0));
-    System.out.println(Snake.get(1));
+    System.out.print(Snake.get(0));
+    System.out.print(Snake.get(1));
     System.out.println(Snake.get(2));
+    System.out.print(Snake.set(0, "Breaking News!  "));
+    System.out.print(Snake.set(1, "The List has changed  "));
+    System.out.print(Snake.set(2, "HAhahahahaha  "));
+    System.out.print(Snake.set(3, "Yeah....  "));
+    System.out.println(Snake.set(4, "pls work!"));
+    System.out.print(Snake.get(0));
+    System.out.print(Snake.get(1));
+    System.out.print(Snake.get(2));
+    System.out.print(Snake.get(3));
+    System.out.println(Snake.get(4));
+    System.out.print(Snake.set(5, "This should crash!"));
   }
 
 }
