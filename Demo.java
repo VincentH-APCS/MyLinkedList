9f11532825f07756bfba10b04ed09acfd82b7122
VincentH-APCS <vhuang10@stuy.edu>
2021-01-06 21:12:58 -0500

    Tested the basic get and set methods for specifically data of a node

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo{
+  public static void main(String[] args){
+    Node alpha = new Node("first");
+    Node beta = new Node("second");
+    Node wild = new Node("IDk lol");
+
+    System.out.println(alpha.getData());
+    System.out.println(beta.getData());
+    System.out.println(wild.getData());
+    System.out.println(wild.setData("change!"));
+    System.out.println(wild.getData());
+  }
+
+}
