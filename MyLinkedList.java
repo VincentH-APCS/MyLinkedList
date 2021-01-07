f00ec3af140bf501ad90f79548fb757490fc9ddb
VincentH-APCS <vhuang10@stuy.edu>
2021-01-06 21:44:32 -0500

    Wrote the code for (boolean) add

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -3,7 +3,32 @@ public class MyLinkedList{
   private Node start, end;
 
   public MyLinkedList(){
-
+    size = 0;
+    start = null;
+    end = null;
   }
+
+  public int size(){
+    return size;
+  }
+
+  public boolean add(String value){
+    Node newone = new Node(value);
+    if (size == 0){
+      start = newone;
+      end = newone;
+    }
+    else if (size == 1){
+      start.setNext(newone);
+      end = newone;
+    }
+    else {
+    Node oldend = end;
+    end = newone;
+    newone.setPrev(oldend);
+   }
+    size = size + 1;
+    return true;
+   }
 
 }
