dc065cb211083ebac9ab37c02588a6b4699bc68f
VincentH-APCS <vhuang10@stuy.edu>
2021-01-06 23:12:06 -0500

    Wrote the first attempt at the code for add(void)

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -31,4 +31,23 @@ public class MyLinkedList{
     return true;
    }
 
+   public void add(int index, String value){
+     if(index < 0 || index >= size){
+       throw new IndexOutOfBoundsException("Index" +index+
+       "cannot be negative nor equal to nor greater than the size");
+     }
+      Node current = Start;
+      Node adder = new Node(value);
+      size = size + 1;
+      for(int i = 0; i < size; i++){
+        if(i == index){
+          Node save = current;
+          adder.setNext(current);
+          adder.setPrev(save.getPrev());
+          i = size;
+        }
+        else current = current.next();
+      }
+      }
+
 }
