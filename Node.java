ba12e41e1dd373ee4da64594fd9ae2b2f921b3c2
VincentH-APCS <vhuang10@stuy.edu>
2021-01-06 20:51:39 -0500

    Fixed it so there are get/set methods for each of the instance variables

--- Node.java
+++ Node.java
@@ -7,12 +7,29 @@ public class Node{
   private String data;
   private Node next,prev;
 
-  public String get(){
+  public String getData(){
     return data;
   }
-  public String set(String value){
+  public String setData(String value){
     String old = data;
     data = value;
     return old;
   }
+  public String getNext(){
+    return next;
+  }
+  public String setNext(String value){
+    String old = data;
+    next = value;
+    return old;
+  }
+  public String getPrev(){
+    return prev;
+  }
+  public String setPrev(String value){
+    String old = prev;
+    prev = value;
+    return old;
+  }
+  
 }
