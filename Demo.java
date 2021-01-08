9b772bdf3da466c29737284fafadddf7b19359ba
VincentH-APCS <vhuang10@stuy.edu>
2021-01-08 14:59:02 -0500

    Testing remove a lot

--- Demo.java
+++ Demo.java
@@ -1,31 +1,31 @@
 public class Demo{
   public static void main(String[] args){
-    Node alpha = new Node("first");
-    Node beta = new Node("second");
-    Node wild = new Node("IDk lol");
 
     MyLinkedList Snake = new MyLinkedList();
 
-    Snake.add(0, "Once upon a time");
-    Snake.add(1, "There lived an");
-    Snake.add(2, "Ancient Wizard who");
-    Snake.add(3, "Ate a cheeseburger");
-    System.out.println(Snake.size());
-    Snake.add(4, "He then went to");
-    Snake.add(4, "at McWizardry Bell");
-    Snake.add(0, "This story is an old one.");
-    Snake.add(7, "the Wands R Us");
-    Snake.add(8, "That was next door");
-    System.out.println(Snake.size());
-    Snake.add(9, "So he could");
-    Snake.add(10, "purchase");
-    Snake.add(11, "2 new wands");
-    Snake.add(12, "one for himself");
-    Snake.add(13, "and one for his");
-    Snake.add(14, "niece's birthday next week.");
+    Snake.add(0, "Hello");
+    //System.out.println(Snake.remove(0));
+    Snake.add(1, "Bonjour");
+    Snake.add(2, "Gutentag");
+    Snake.add(3, "Konichiwa");
+    Snake.add(4, "Goodbye");
+    Snake.add(5, "Hola");
+    Snake.add(6, "This shouldn't be here");
     System.out.println(Snake.size());
     System.out.println(Snake.toString());
+    System.out.println(Snake.toStringReversed());
     System.out.println();
-    System.out.println(Snake.reverseString());
+    System.out.println(Snake.remove(4));
+    System.out.println(Snake.toString());
+    System.out.println(Snake.remove(5));
+    System.out.println(Snake.size());
+    System.out.println(Snake.toString());
+    System.out.println(Snake.toStringReversed());
+    System.out.println(Snake.remove(0));
+    System.out.println();
+    System.out.println(Snake.size());
+    System.out.println(Snake.toString());
+    System.out.println(Snake.toStringReversed());
+
 }
 }
